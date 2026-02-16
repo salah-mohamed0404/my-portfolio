089d9999ffa42159c49913d67b169c1b9363ed19
Salah Mohamed <salah.mohamed0404@gmail.com>
2026-02-16 17:14:00 +0200

    feat: add my contact info

---
+++ src/constants/contact-info.ts
@@ -0,0 +1,10 @@
+export const CONTACT_INFO = {
+	phone: "+201514148889",
+	email: "salah.mohamed0404@gmail.com",
+	address: "Giza, Egypt",
+	linkedin: "https://linkedin.com/in/salah-mohamed0404",
+	github: "https://github.com/salah-mohamed0404",
+	qabilah: "https://qabilah.com/profile/salah-mohamed",
+	askMe:
+		"https://qabilah.com/profile/salah-mohamed/professional-profile?target=ask-me-anything",
+} as const;
